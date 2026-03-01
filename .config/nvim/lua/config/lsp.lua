3d8f5b9c1c2bd604b1b7da3f9820c6f5a4a80bb6
Pedro Augusto <pedro.aug2808@gmail.com>
2026-03-01 16:05:53 -0300

    wip

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -1,3 +1,11 @@
+local dir_content_str = vim.fn.glob("$HOME/.config/nvim/lua/lsps/*")
+local dir_content_list = vim.split(dir_content_str, '\n', {trimempty=true})
+local lsps = {}
+
+for i, lsp_path in pairs(dir_content_list) do
+    local split_path = vim.split(lsp_path, "/")
+end
+
 Servers = {
     "clangd",
     "css_variables",
@@ -10,6 +18,11 @@ Servers = {
     "tailwindcss",
     "ts_ls",
 }
+print("LSPs:")
+for i, file_path in ipairs(lsps) do
+    print(lsps)
+end
+
 
 for _, server in ipairs(Servers) do
     require("lsps." .. server)
